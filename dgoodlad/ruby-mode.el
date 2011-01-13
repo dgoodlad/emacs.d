74a416f98863b531565611495754e41937c0d592
David Goodlad <david@goodlad.ca>
2011-01-14 10:58:40 +1100

    Automatically set ruby-mode for Rakefile etc

--- dgoodlad/ruby-mode.el
+++ dgoodlad/ruby-mode.el
@@ -2,5 +2,12 @@
 
 (add-hook 'ruby-mode-hook
           (lambda ()
+	    (define-tab-width 2)
             (ruby-electric-mode)
             ))
+
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("autotest$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("irbrc$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("rake$" . ruby-mode))
