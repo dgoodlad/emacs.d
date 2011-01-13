4ec543efb049429db1debef4383921aa6de24817
David Goodlad <david@goodlad.ca>
2011-01-13 16:07:28 +1100

    Use zenburn

--- dgoodlad/color-theme.el
+++ dgoodlad/color-theme.el
@@ -1,6 +1,7 @@
 ;;; Pretty colors
 
 (color-theme-initialize)
-(vendor 'color-theme-ir-black)
-(color-theme-ir-black)
-
+;;(vendor 'color-theme-ir-black)
+;;(color-theme-ir-black)
+(vendor `zenburn)
+(color-theme-zenburn)
