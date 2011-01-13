900a9fed2bfb9f248d30513c6614087373208acc
David Goodlad <david@goodlad.ca>
2011-01-14 10:58:06 +1100

    Fix viper customizations

--- dgoodlad/dot-viper.el
+++ dgoodlad/dot-viper.el
@@ -1,18 +1,18 @@
 ;; Don't use vi-style autoindent
-(setq 'viper-auto-indent nil)
+(setq viper-auto-indent nil)
 
 ;; Enable electric autoindent
-(setq 'viper-electric-mode t)
+(setq viper-electric-mode t)
 
 ;; Enable regex search
-(setq 'viper-re-search t)
+(setq viper-re-search t)
 
 ;; :set sw
-(setq 'viper-shift-width 2)
+(setq viper-shift-width 2)
 
 ;; Enable C-h for standard emacs help
-(setq 'viper-want-ctl-h-help t)
+(setq viper-want-ctl-h-help t)
 
 ;; Disable vi-style keybindings in minibuffer
-(setq 'viper-vi-style-in-minibuffer nil)
+(setq viper-vi-style-in-minibuffer nil)
 
