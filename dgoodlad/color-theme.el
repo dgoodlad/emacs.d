27703aefd236b5d38b1f7daa47460f8e67ff7263
David Goodlad <david@goodlad.ca>
2011-01-14 16:12:28 +1100

    Try out the railscasts color theme

--- dgoodlad/color-theme.el
+++ dgoodlad/color-theme.el
@@ -1,7 +1,10 @@
 ;;; Pretty colors
 
 (color-theme-initialize)
-;;(vendor 'color-theme-ir-black)
-;;(color-theme-ir-black)
-(vendor `zenburn)
-(color-theme-zenburn)
+;(vendor 'color-theme-ir-black)
+;(color-theme-ir-black)
+;(vendor `zenburn)
+;(color-theme-zenburn)
+;(vendor 'color-theme-railscasts)
+(load-file "~/.emacs.d/vendor/color-theme-railscasts/color-theme-railscasts.el")
+(color-theme-railscasts)
