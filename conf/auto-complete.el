954469088aa367630cbf3152f8b325fd53c2dcc2
David Goodlad <david@goodlad.ca>
2011-06-01 14:16:31 +1000

    Make auto-complete co-exist with viper-mode

--- conf/auto-complete.el
+++ conf/auto-complete.el
@@ -1,3 +1,13 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; This stops "end" followed by "RET" getting completed to something
+;; like "endomorph" - have to use an explicit "TAB" to complete.
+(define-key ac-complete-mode-map (kbd "\r") nil)
+
+;; Make auto-complete co-exist with viper-mode nicely
+(when *viper-enabled*
+  (define-key ac-complete-mode-map (kbd "C-n") 'dabbrev-expand)
+  (define-key ac-complete-mode-map (kbd "C-p") 'dabbrev-expand)
+  (define-key ac-complete-mode-map viper-ESC-key 'viper-intercept-ESC-key))
