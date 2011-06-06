cc9847fa099d12affc280fe641ec04608ced887b
David Goodlad <david@goodlad.ca>
2011-06-06 14:50:07 +1000

    Include color-theme and reorder packages

--- packages.el
+++ packages.el
@@ -4,11 +4,13 @@
       '((:name package
                :after (lambda()
                         (setq package-archives '("tromey" . "http://tromey.com/elpa/"))))
+        (:name color-theme
+               :type git
+               :url "https://github.com/emacsmirror/color-theme.git")
         (:name color-theme-solarized
-               :load "solarized-definitions.el"
                :after (lambda()
-                        (require 'color-theme-solarized)
                         (color-theme-solarized-dark)))
+        (:name color-theme-zenburn)
         (:name vimpulse
               :after (lambda()
                        (load "conf/dot-viper")
@@ -20,30 +22,22 @@
                :type elpa
                :load "inf-ruby.el")
         (:name ruby-compilation)
-        (:name css-mode :type elpa)
-        ;(:name textmate
-        ;       :type git
-        ;       :url "https://github.com/defunkt/textmate.el.git"
-        ;       :load "textmate.el"
-        ;       :after (lambda() (textmate-mode)))
-        ;(:name rvm
-        ;       :type git
-        ;       :url "http://github.com/djwhitt/rvm.el.git"
-        ;       :load "rvm.el"
-        ;       :compile ("rvm.el")
-        ;       :after (lambda() (rvm-use-default)))
         (:name rhtml-mode)
+        (:name css-mode :type elpa)
         (:name yaml-mode)
         (:name auto-complete
                :after (lambda () (load "conf/auto-complete")))
         (:name paredit)
         (:name coffee-mode)
-        (:name markdown-mode
-               :after (lambda ()
-                        (setq markdown-mode-hook 'turn-on-auto-fill)))
         (:name magit)
         (:name magithub)
         (:name nxhtml)
+        (:name js2-mode
+               :type git
+               :url "https://github.com/mooz/js2-mode")
+        (:name markdown-mode
+               :after (lambda ()
+                        (setq markdown-mode-hook 'turn-on-auto-fill)))
         (:name org-mode
                :url "git://orgmode.org/org-mode.git"
                :after (lambda ()
