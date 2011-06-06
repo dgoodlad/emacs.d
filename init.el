73c5f4d662516e62b2b04a78b740ed9d9210fa5d
David Goodlad <david@goodlad.ca>
2011-06-06 14:50:37 +1000

    Include gtd in org-mode agenda

--- init.el
+++ init.el
@@ -18,3 +18,15 @@
 ; Actual package list - hand over, it will load the configs on a per package
 ; basis
 (load "packages")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/gtd.org"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
