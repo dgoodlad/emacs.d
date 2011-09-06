d49dbd3bdcb3f66ac93f50012343e64713fe2eca
David Goodlad <david@goodlad.ca>
2011-09-07 08:02:39 +1000

    Fix terminal colors

--- packages.el
+++ packages.el
@@ -8,6 +8,8 @@
         (:name multi-term
                :after (lambda()
                         (setq multi-term-program "/bin/zsh")
+                        (setq term-default-bg-color nil)
+                        (setq term-default-fg-color nil)
                         (global-set-key (kbd "C-c t") 'multi-term-next)
                         (global-set-key (kbd "C-c T") 'multi-term)))
         (:name ruby-mode
