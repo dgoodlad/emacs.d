4cc884fb8ef0eac4ea108f770f6b5720fd18082d
David Goodlad <david@goodlad.ca>
2011-09-22 21:34:44 +1000

    Use jslint-style `global` directive to identify global vars
    `js2-mode` (rightly) complains about undeclared variables, but sometimes
    these are meant to be referring to globals on purpose. Using a directive
    like
    
    ```
    /* global Foo Bar baz */
    ```
    
    at the top of a js file tells `js2-mode` not to complain about those
    variables

---
+++ conf/init-js2-mode.el
@@ -0,0 +1,11 @@
+;; After js2 has parsed a js file, we look for jslint globals decl comment ("/* global Fred, _, Harry */") and
+;; add any symbols to a buffer-local var of acceptable global vars
+(add-hook 'js2-post-parse-callbacks
+          (lambda ()
+            ;; strip newlines etc so the regexp below will match a multiline comment
+            (let ((btext (replace-regexp-in-string "[\n\t ]+" " " (buffer-substring-no-properties 1 (buffer-size)) t t)))
+              (setq js2-additional-externs
+                    (split-string
+                     (if (string-match "/\\* *global \\(.*?\\)\\*/" btext) (match-string-no-properties 1 btext) "")
+                     "[ ,]+" t))
+              )))
