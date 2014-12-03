5a50cb3a652c796f918d7c0fc36856bdb1051499
David Goodlad <david@goodlad.net>
2014-12-03 18:07:10 +1100

    Configure smartparens

--- init.el
+++ init.el
@@ -120,4 +120,16 @@
 ;; -----------------------------------------------------------------------------
 
 (require 'smartparens-config) ; Use the default smartparens config
-(smartparens-global-mode 1)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+
+;; -----------------------------------------------------------------------------
+;; Programming modes
+;; -----------------------------------------------------------------------------
+
+(add-hook 'prog-mode-hook (lambda ()
+                            (smartparens-mode 1)))
